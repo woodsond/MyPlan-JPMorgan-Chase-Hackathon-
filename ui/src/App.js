4471ddfc58b30f18164e902a95366783cc65a3b2
Diante Woodson <47933046+woodsond@users.noreply.github.com>
2020-09-26 05:51:31 -0500

    Redid Navbar. Added functionality between the navbar links.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,71 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CustomNavBar from "./components/customNavBar";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Piggy from "./pages/Piggy";
+import Articles from "./pages/Articles"
+import CreditCards from "./pages/CreditCards"
 
-//Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Router 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-//Pages
-import Home from './pages/Home';
-import Articles from './pages/Articles';
-import CreditCards from './pages/CreditCards';
-import Piggy from './pages/Piggy';
-
-//Compontents
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-
-class App extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-       title: 'myPath',
-       headerLinks:
-       [
-         { title: 'myPiggy', path: "/piggyBank"},
-         { title: 'Article', path: "/article"},
-         { title: 'Credit Cards', path: "/creditcards"}
-       ],
-       home: {
-         title: "Home",
-       },
-       piggyBank: {
-          title: "My Piggy Bank",
-       },
-       articles: {
-         title: "Articles",
-       },
-       creditCards: {
-         title: "creditCards",
-       }
-    }
-  }
-  render(){
-    return(
-      <Router>
-        <Container fluid={true}>
-          <Navbar bg="transparent" expand="lg">
-            <Navbar.Toggle aria-controls="navbar-toggle"/>
-            <Navbar.Collapse id="navbar-toggle">
-              <Nav className="ml-auto">
-              <h5> <Link className="nav-link navtext" to="/">Home</Link></h5>
-                  <h5> <Link className="nav-link navtext" to="/piggyBank">myPiggy</Link></h5>
-                  <h5> <Link className="nav-link navtext" to="/articles">Articles</Link></h5>
-                  <h5> <Link className="nav-link navtext" to="/creditCards">Credit Cards</Link></h5>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Route path="/" exact render={() => <Home title={this.state.home.title} />} />
-          <Route path="/piggyBank" exact render={() => <Piggy title={this.state.piggyBank.title}/>} />
-          <Route path="/articles" render={() => <Articles title={this.state.articles.title} />} />
-          <Route path="/creditCards" render={() => <CreditCards title={this.state.creditCards.title} />} /> 
-        </Container>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+    <div className="Options">
+      <CustomNavBar />
+      <Route path="/MyPiggy" render={() => {
+        return (
+          <>
+            <Piggy title={"MyPiggy"}/>
+          </>
+        )
+      }} />
+      <Route path="/Articles" render={() => {
+        return (
+          <>
+            <Articles title={"Financial Articles"}/>
+          </>
+        )
+      }} />
+      <Route path="/CreditCards" render={() => {
+        return (
+          <>
+            <CreditCards title={"Credit Cards"}/>
+          </>
+        )
+      }}/>
+    </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ ui/src/components/customNavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import * as ReactBootStrap from "react-bootstrap";
+
+function CustomNavBar() {
+  return (
+    <div className="MyPath Virtual Series">
+      <ReactBootStrap.Navbar
+        collapseOnSelect
+        expand="xl"
+        bg="success"
+        variant="light"
+      >
+        <ReactBootStrap.Navbar.Brand href="#home">
+          MyPath Virtual Series
+        </ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-contorls="responsive-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+          <ReactBootStrap.Nav className="mr-auto">
+            <ReactBootStrap.Nav.Link href="MyPiggy">
+              MyPiggy
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="CreditCards">
+              Credit Cards
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="Articles">
+              Financial Articles
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link aria-disabled="true">
+              Powering youth potential, seeding economnic mobility!
+            </ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Navbar>
+    </div>
+  );
+}
+
+export default CustomNavBar;
